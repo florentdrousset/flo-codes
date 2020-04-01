276449fef3519b247498b7a94d014c3823a3e20e
florent <fdrousset@sqli.com>
2020-04-01 21:47:09 +0200

    login created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,12 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -13,7 +17,7 @@ class HomeController extends AbstractController
      * @return string
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
     {
         return $this->render('home.html.twig');
     }
