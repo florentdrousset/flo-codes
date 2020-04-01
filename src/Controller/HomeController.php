ff2f0804368178dbac79a1bc676c0ee1a42f1576
florent <fdrousset@sqli.com>
2020-04-01 20:16:02 +0200

    HomeController created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,17 @@
 namespace App\Controller;
 
 
-class HomeController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController extends AbstractController
+{
+    /**
+     * @return string
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home.html.twig');
+    }
 }
